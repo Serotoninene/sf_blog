e41ebb02d182f1afe0139c1679973e8aaa3c3e57
Serotoninene <pujol.alexandre@hotmail.fr>
2020-11-24 14:51:42 +0100

    ajout des pages article et category

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -13,14 +13,37 @@
 
         /**
          * @Route("/articles", name="list_articles")
+         *
+         * On rajoute la classe ArticleRepository directement dans la ligne de la fonction et on la sauvegarde dans une
+         * variable --> AUTOWIRE
          */
         public function listArticles(ArticleRepository $articleRepository){
 
+            /**
+             * Grace à ArticleRepository je peux faire des requêtes SELECT SQL et retrouver les éléments désirés dans
+             *ma BDD, en l'occurence je les cherche tous
+             */
             $articles = $articleRepository->findAll();
 
             return $this->render('articles.html.twig',[
             'articles' => $articles
             ]);
+        }
+
+        /**
+         *
+         * id est ci-dessous une WILDCARD, ie c'est une variable, dans ce cas modifiée en fonction du lien sur lequel on a cliqué
+         *
+         * @Route ("/articles/{id}", name="show_article")
+         */
+        public function showArticle($id, ArticleRepository $articleRepository){
+
+            $article = $articleRepository->find($id);
+            return $this->render("article.html.twig",
+            [
+                'article' => $article
+            ]);
+
         }
 
         /**
@@ -35,6 +58,19 @@
             ]);
 
         }
+        /**
+        * @Route ("/categories/{id}", name="show_category")
+        */
+        public function showCategory($id, CategoryRepository $categoryRepository){
+
+            $category = $categoryRepository->find($id);
+            return $this->render("category.html.twig",
+                [
+                    'category' => $category
+                ]);
+
+        }
+
 
     }
 
